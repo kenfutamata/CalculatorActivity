e4edd4cdaf04a743ffe12a3432c3d780ee820235
kenfutamata <104425231+kenfutamata@users.noreply.github.com>
2024-02-01 15:05:09 +0800

    Add files via upload

---
+++ Calculator Activity/Calculator Activity/calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Activity
+{
+    public class calculator
+    {
+        //the datatypes will be under a private setting to hide all the numerical contents inputted by thr user
+        private double num1;
+        private double num2;
+
+        //using the constructor ith an arrgument 
+        public calculator(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        //using access modifier oe the getters format
+        public double getNum1()
+        {
+            return num1;
+        }
+
+        public double getNum2()
+        {
+            return num2;
+        }
+
+        //program will compute the 2 numbers inputted by the user
+        
+        //sum or addition
+        public double sum()
+        {
+            double sum = getNum1() + getNum2();
+            return sum; 
+        }
+        //subtraction
+        public double sub()
+        {
+            double sub = getNum1() - getNum2();
+            return sub; 
+        }
+        //multiplication
+
+        public double multi()
+        {
+            double multi = getNum1() * getNum2();
+            return multi;
+        }
+
+        //divide
+        public double divide()
+        {
+            double divide = getNum1() / getNum2();
+            return divide; 
+        }
+        //end of program
+    }
+}
---
+++ Calculator Activity/Calculator Activity/calculatortest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Activity
+{
+    public class calculatortest
+    {
+ 
+        public static void Main(string[] args)
+        {
+          //calling the main part of the calculator
+            calculatormain();
+        }
+        public static void calculatormain()
+        {
+            double num1;
+            double num2;
+            String opera;//operation
+           
+            //continous looping of data until user will input exit to exit the program.
+            while(true)
+            {
+                
+                //input functions to allow user to input any numerical number       
+                Console.WriteLine("Enter num1: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter num2: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+                //function call to call from the caluclator.cs
+                calculator cal = new calculator(num1, num2);
+                //the user will input which operator the user wants to compute both numbers
+                Console.WriteLine("+ for addition \n - for subtraction \n * for multiplcation \n / for division \n and exit for exiting the program ");
+                Console.WriteLine("Enter a opperation");
+                opera = Console.ReadLine();
+                switch (opera)
+                {
+                    case "+":
+                        //addition function call and display
+                        Console.WriteLine("Sum is :"+ cal.sum());
+                       
+                        break;
+                    case "-":
+                        //subtraction function call and display
+                      Console.WriteLine("Sub is "+ cal.sub());
+                       
+                        break;
+                    case "*":
+                        //multiplcation function call and display
+                        Console.WriteLine("Multi is "+cal.multi());
+                        break;
+
+                    case "/":
+                        //division function call and display
+                        Console.WriteLine("Divide is "+cal.divide());
+                        break;
+                        
+                    case "exit": 
+                        //the program will exit if the user inputs exit on the console
+                            Environment.Exit(0); 
+                          break; 
+                }
+                //user will be questioned if the user wants to continue the program or will exit the program by typing yes or no
+                Console.WriteLine("Do you want to continue yes or no?");
+                String choosesum = Console.ReadLine();
+
+                if (choosesum.Equals("yes"))//if yes, the program will continue
+                {
+                    continue;
+                }
+                else if (choosesum.Equals("no"))//if no, the program will stop 
+                {
+                    Environment.Exit(0);
+                }
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
